6cf27cb941afc9b8e724ff601e2f6392392a440e
Maico Timmerman <maico.timmerman@gmail.com>
2017-04-06 12:54:03 +0200

    More tests

---
+++ nested_funs/functional/civic.h
@@ -0,0 +1,13 @@
+#ifndef __CIVIC_H
+#define __CIVIC_H
+
+extern void printInt(int val);
+extern void printFloat(float val);
+
+extern int scanInt();
+extern float scanFloat();
+
+extern void printSpaces(int num);
+extern void printNewlines(int num);
+
+#endif
